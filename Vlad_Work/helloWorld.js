dba05b9441ec2bff27d6272fbcd10022b388ee85
VladToncu <toncuvlad@gmail.com>
2015-03-29 07:46:10 +0100

    Showing nearby attractions and features plus working based on interests

--- Vlad_Work/helloWorld.js
+++ Vlad_Work/helloWorld.js
@@ -10,8 +10,13 @@ work=localStorage.getItem("work").toLowerCase();
 }
 var work;
 var hotel;
+var coffee;
+var food;
+var bar;
 var map;
 var infowindow;
+var latlng;
+var pyrmont;
 
 var clickedWorkPlace=false;
 
@@ -19,12 +24,12 @@ function showMap(position) {
 
 var latitude  = position.coords.latitude;
 var longitude = position.coords.longitude;
-var latlng    = new google.maps.LatLng(latitude, longitude);
+latlng    = new google.maps.LatLng(latitude, longitude);
 var mapOpts   = {zoom: 15, center: latlng, mapTypeId: google.maps.MapTypeId.HYBRID};
 var canvas    = document.getElementById('map_canvas');
 map = new google.maps.Map(canvas, mapOpts);
 
-var pyrmont=new google.maps.LatLng(latitude,longitude);
+pyrmont=new google.maps.LatLng(latitude,longitude);
 
 var marker    = new google.maps.Marker({position: latlng, map: map});
 var dirServ   = new google.maps.DirectionsService();
@@ -37,20 +42,27 @@ dirDisp.setDirections(result);});
 });
 infowindow = new google.maps.InfoWindow();
 
-var request1 = {
+var requestGeneral = {
     location: pyrmont,
     radius: 2000,
     types: ['establishment']
   };
-var request2 = {
+var requestHotel = {
     location: pyrmont,
     radius: 2000,
     types: ['lodging']
   };
-  
 var service = new google.maps.places.PlacesService(map);
-service.nearbySearch(request1, callback);
-service.nearbySearch(request2, callback);
+service.nearbySearch(requestGeneral, callback_important);
+service.nearbySearch(requestHotel, callback_important);
+if(localStorage.getItem("food")=="food")
+	searchFood();
+if(localStorage.getItem("food")=="food")
+	searchRestaurant();
+if (localStorage.getItem("bar")=="bar")
+	searchBar();
+if(localStorage.getItem("coffee")=="coffee")
+	searchCoffee();
 } 
 var walkingMode="DRIVING";
 function changeMode()
@@ -65,7 +77,7 @@ function setData()
 	localStorage["work"]=document.getElementById("work").value;
 }
 var foundHotel=false;
-function callback(results, status) {
+function callback_important(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
     foundHotel=false;
@@ -87,7 +99,84 @@ function createMarker(place) {
   });
 
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
+  if(place.rating!=null)
+    infowindow.setContent(place.name + " <br></br>   " + place.rating + "/5");
+  else
+        infowindow.setContent(place.name + " <br></br>   " + "Unrated");
     infowindow.open(map, this);
   });
+}
+function searchFood()
+{
+	var requestFood = {
+    location: pyrmont,
+    radius: 1500,
+    types: ['food']
+  };
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(requestFood, callback_auxiliar);
+}
+function searchRestaurant()
+{
+	var requestRestaurant = {
+    location: pyrmont,
+    radius: 1500,
+    types: ['restaurant']
+  };
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(requestRestaurant, callback_auxiliar);
+}
+function callback_auxiliar(results, status) 
+{
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+    	for (var i = 0; i < results.length; i++) 
+    	{
+    		if(results[i].rating>3.5 && results[i].open_now)
+    			createMarker(results[i]);
+    	}
+    	}
+}
+function searchBar()
+{
+	var requestBar = {
+   		 location: pyrmont,
+    		 radius: 1500,
+    		 types: ['bar']
+  			};
+  	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(requestBar, callback_auxiliar);
+}
+function searchCoffee()
+{
+	var requestCoffee = {
+    			location: pyrmont,
+    			radius: 2000,
+    			types: ['cafe']
+  			};
+  	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(requestCoffee, callback_auxiliar);
+}
+function getCoffee()
+{
+	var coffeeChecker=document.getElementById("coffee");
+	if(coffeeChecker.checked)
+		localStorage["coffee"]="coffee";
+	else
+		localStorage["coffee"]="notcoffee";
+}
+function getFoodandRestaurant()
+{
+	localStorage["food"]="notfood";
+	var foodChecker=document.getElementsByClassName("eating")
+	for(var i=0;i<foodChecker.length;i++)
+		if(foodChecker[i].checked)
+			localStorage["food"]="food";
+}
+function getBar()
+{
+	var barChecker=document.getElementById("bar");
+	if(barChecker.checked)
+		localStorage["bar"]="bar";
+	else
+		localStorage["bar"]="notbar";	
 }
